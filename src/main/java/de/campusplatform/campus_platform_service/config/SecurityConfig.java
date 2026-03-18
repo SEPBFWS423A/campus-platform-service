55eb8bf972bd2d64e4be0e5cd0b0d5724e402fb8
DerPatrice <patricelechner88@gmail.com>
2026-03-18 21:28:48 +0100

    added the api domain to cors allowedOrigins

--- src/main/java/de/campusplatform/campus_platform_service/config/SecurityConfig.java
+++ src/main/java/de/campusplatform/campus_platform_service/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://campusplatform.de"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://campusplatform.de", "https://api.campusplatform.de"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
