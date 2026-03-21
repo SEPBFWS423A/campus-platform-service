99813f673d3319241532a269a46daf8e1ef8ffd3
DerPatrice <patricelechner88@gmail.com>
2026-03-21 22:40:36 +0100

    added endpoints and logic for the list of all users, the deleting of users, the user stats and the editing of users, added that the UserController can only be accesed from authenticated users

--- src/main/java/de/campusplatform/campus_platform_service/controller/AdminController.java
+++ src/main/java/de/campusplatform/campus_platform_service/controller/AdminController.java
@@ -3,6 +3,7 @@ package de.campusplatform.campus_platform_service.controller;
 import de.campusplatform.campus_platform_service.dto.AdminUserResponse;
 import de.campusplatform.campus_platform_service.dto.AdminUserUpdateRequest;
 import de.campusplatform.campus_platform_service.dto.InvitationRequest;
+import de.campusplatform.campus_platform_service.dto.UserStatsResponse;
 import de.campusplatform.campus_platform_service.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -32,9 +33,20 @@ public class AdminController {
         return ResponseEntity.ok(authService.getAllUsers());
     }
 
+    @GetMapping("/users/stats")
+    public ResponseEntity<UserStatsResponse> getUserStats() {
+        return ResponseEntity.ok(authService.getUserStats());
+    }
+
     @PutMapping("/users/{id}")
     public ResponseEntity<Void> updateUser(@PathVariable Long id, @RequestBody AdminUserUpdateRequest request) {
         authService.updateUser(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        authService.deleteUser(id);
         return ResponseEntity.ok().build();
     }
 }
